9723f282c41ea28b5a57cec5cda372743744b2d0
BuckShlegeris <buckmbs@hotmail.com>
2013-07-30 16:14:13 +1000

    Finished the first version of the backend

--- Backend.py
+++ Backend.py
@@ -3,6 +3,8 @@
 from Dimensions import Dimensions
 from Equation import Equation
 from sympy import pretty
+from sympy.solvers import solve
+from sympy import Symbol
 
 class Backend():
     """The backend for my graphical equation manipulator.
@@ -61,6 +63,11 @@ class Backend():
         for var in newUnits:
             self.dimensions[var] = newUnits[var]
 
+    def findEquationWithVar(self,var):
+        for equation in self.equations:
+            if var in equation.getVars():
+                return equation
+        raise Exception("Var not found in any equation")
 
     def addEquivalency(self,newEquivalencies):
         newgroup = list(newEquivalencies)
@@ -75,6 +82,20 @@ class Backend():
         self.equivalencies = outlist
         self.checkUnits()
 
+    def varsEqual(self,var1,var2):
+        for group in self.equivalencies:
+            if var1 in group:
+                return var2 in group
+        return False
+
+    def equivalenciesOfVariable(self,var):
+        for group in self.equivalencies:
+            if var in group:
+                out = list(group)
+                out.remove(var)
+                return out
+        return []
+
     def checkUnits(self):
         print "We're being dodgy and not checking.dimensions!"
         return
@@ -82,20 +103,21 @@ class Backend():
             if len(group)>1: # I'm pretty sure this should always be true
                 firstThing = group[0]
                 for thing in group[1:]:
-                    assert self.dimensions[firstThing] == self.dimensions[thing]
+                    assert (self.dimensions[firstThing] ==
+                                                 self.dimensions[thing])
 
     def findExpression(self,var,equation):
         if var in equation.getVars():
-            exp = equation.solve(var)
-            if exp:
-                self.expressions[var] = exp
+            exps = equation.solve(var)
+            if exps:
+                self.expressions[var] = exps
             else:
                 raise Exception("No solution found")
             return
         raise Exception("Variable not in expression")
 
 
-    # Not tested!
+    # Not tested
     def updateExpressionsWithEquivalency(self):
         newEquivalency = self.equivalencies[-1]
 
@@ -133,20 +155,67 @@ class Backend():
             self.expressions[varToChange].append(
                                         exp.replace(oldName,newName))
 
-    def rewriteUsingExpression(self,equation,var,expr):
-        raise NotImplementedError()
+    def rewriteUsingExpression(self,var,varToRemove,varWhoseExpToUse):
+        """
+        Rewrites the expression for var to not include varToRemove,
+        by substituting the expression for varWhoseExpToUse into it.
+        """
 
-    def rewriteUsingEquation(self,equation,var,equation2):
-        raise NotImplementedError()
+        assert self.varsEqual(varToRemove,varWhoseExpToUse)
+
+        newExpList = []
+
+        for exp in self.expressions[var]:
+            for exp2 in self.expressions[varWhoseExpToUse]:
+                newExpList.append(exp.replace(varToRemove,exp2))
+
+        self.expressions[var] = newExpList
+
+
+    def rewriteUsingEquation(self,var,varToRemove,equation):
+        """
+        Rewrites the expression for var to not include varToRemove,
+        by solving equation for varToRemove, then substituting that
+        into the expression for var.
+        """
+
+        equat = equation.equation
+        for var2 in self.equivalenciesOfVariable(varToRemove):
+            equat = equat.replace(var2,varToRemove)
+
+        exps = solve(equat,varToRemove)
+
+        outexps = []
+
+        for exp1 in self.expressions[var]:
+            for exp2 in exps:
+                outexps.append(exp1.replace(varToRemove,exp2))
+
+
+        self.expressions[var] = [self.unifyVarsInExpression(x)
+                        for x in outexps]
+
+    def unifyVarsInExpression(self,exp):
+        varNameList = [x.name for x in exp.atoms(Symbol)]
+        for var in varNameList:
+            for var2 in self.equivalenciesOfVariable(var):
+                exp = exp.replace(var2,var)
+        exp.simplify()
+        return exp
+
+
 
 if __name__ == '__main__':
     a = Backend()
     a.addEquation(Equation("KE","0.5*m*v**2"),{})
-    a.addEquation(Equation("KE","0.5*m*v**2"),{})
+    a.addEquation(Equation("PE","m*g*h"),{})
     a.addEquivalency(["m","m2"])
+    a.addEquivalency(["KE","PE"])
 
     a.findExpression("v",a.equations[0])
 
-    a.rotateVariableInExpression("v","m")
+    a.show()
+
+    a.rewriteUsingEquation("v","KE",a.equations[1])
 
     a.show()
